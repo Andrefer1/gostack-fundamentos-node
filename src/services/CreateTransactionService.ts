ba6067d92e45320dfdb17d98d6cd7eb32377c7f8
André F. Bispo <andrefer483@gmail.com>
2020-07-07 02:31:48 -0300

    challenge finalized

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,16 +1,38 @@
-import TransactionsRepository from '../repositories/TransactionsRepository';
-import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository'
+import Transaction from '../models/Transaction'
 
-class CreateTransactionService {
-  private transactionsRepository: TransactionsRepository;
-
-  constructor(transactionsRepository: TransactionsRepository) {
-    this.transactionsRepository = transactionsRepository;
-  }
-
-  public execute(): Transaction {
-    // TODO
-  }
+interface Request {
+    title: string
+    value: number
+    type: 'income' | 'outcome'
 }
 
-export default CreateTransactionService;
+class CreateTransactionService {
+    private transactionsRepository: TransactionsRepository
+
+    constructor(transactionsRepository: TransactionsRepository) {
+        this.transactionsRepository = transactionsRepository
+    }
+
+    public execute({ title, value, type }: Request): Transaction {
+        if (!['income', 'outcome'].includes(type)) {
+            throw new Error('Transaction type is invalid')
+        }
+
+        const { total } = this.transactionsRepository.getBalance()
+
+        if (type === 'outcome' && total < value) {
+            throw new Error('You do not have enough balance')
+        }
+
+        const transaction = this.transactionsRepository.create({
+            title,
+            value,
+            type,
+        })
+
+        return transaction
+    }
+}
+
+export default CreateTransactionService
